645027a1e5b1d35a477b22e459e212632e3dcfc4
Sharon Arana <shar_mdza@hotmail.com>
2023-05-03 15:49:56 -0600

    Creación de métodos para mejorar consultas a los modelos

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,9 +12,20 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    # Ayuda a que nos de el texto de las preguntas y no un 'object'
+    def __str__(self) -> str:
+        return self.question_text
 
-class Choices(models.Model):
+    # Método personalizado
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
     # En caso de eliminar la pregunta, también se eliminan sus respuestas en cascada
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
