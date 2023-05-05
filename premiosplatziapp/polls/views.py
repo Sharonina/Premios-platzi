87163a4912ce8e9aca58b90d7ce68a5fa671fb62
Sharon Arana <shar_mdza@hotmail.com>
2023-05-05 13:55:50 -0600

    Implementando votos

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -17,7 +17,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f'Estás viendo los resultados de la pregunta número {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -25,11 +26,11 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return (request, 'polls/detail.htm', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': 'No elegiste una respuesta'
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
