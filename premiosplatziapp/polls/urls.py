455063db8ddaebe5da86561e0d20e2d3bde937f4
Sharon Arana <shar_mdza@hotmail.com>
2023-05-05 09:52:55 -0600

    Implementación de vistas

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    # /polls/
+    path("", views.index, name='index'),
+    # /polls/5
+    path('<int:question_id>', views.detail, name='index'),
+    # /polls/results/5
+    path('results/<int:question_id>/', views.results, name='index'),
+    # /polls/vote/5
+    path('vote/<int:question_id>/', views.vote, name='index'),
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-  return HttpResponse('Hello world')
+    return HttpResponse('Bienvenido a los premios Platzi')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'Estás viendo la pregunta número {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'Estás viendo los resultados de la pregunta número {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Estás votando a la pregunta número {question_id}')
