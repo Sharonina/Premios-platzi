b322ed994cbd7fd42a107cf9f47509d304101912
Sharon Arana <shar_mdza@hotmail.com>
2023-05-05 15:35:27 -0600

    Optimización con generic views

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -7,11 +7,11 @@ app_name = 'polls'
 
 urlpatterns = [
     # /polls/
-    path("", views.index, name='index'),
+    path("", views.IndexView.as_view(), name='index'),
     # /polls/5
-    path('<int:question_id>/', views. detail, name="detail"),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
     # /polls/results/5
-    path('results/<int:question_id>/', views.results, name='results'),
+    path('results/<int:pk>/', views.ResultView.as_view(), name='results'),
     # /polls/vote/5
     path('vote/<int:question_id>/', views.vote, name='vote'),
 ]
