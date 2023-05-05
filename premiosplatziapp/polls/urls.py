e85062444e1be733bae9fdf2cca5f6ee1c6825d4
Sharon Arana <shar_mdza@hotmail.com>
2023-05-05 11:49:38 -0600

    Optimización de urls

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'polls'
+
+
 urlpatterns = [
     # /polls/
     path("", views.index, name='index'),
     # /polls/5
-    path('<int:question_id>', views.detail, name='index'),
+    path('<int:question_id>/', views. detail, name="detail"),
     # /polls/results/5
-    path('results/<int:question_id>/', views.results, name='index'),
+    path('results/<int:question_id>/', views.results, name='results'),
     # /polls/vote/5
-    path('vote/<int:question_id>/', views.vote, name='index'),
+    path('vote/<int:question_id>/', views.vote, name='vote'),
 ]
